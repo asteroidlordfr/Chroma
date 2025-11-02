e8a834d00d3f407a205bb438589b2a1a0ce0dc44
AsteroidLord <84286534+asteroidlordfr@users.noreply.github.com>
2025-11-02 11:09:07 +0800

    Update Chroma.lua

--- Source/Chroma.lua
+++ Source/Chroma.lua
@@ -1,4 +1,22 @@
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/asteroidlordfr/Chroma/refs/heads/main/Source/Rayfield.lua'))()
+
+-- the variable (and function) warehouse
+
+local ReplicaSignal = game:GetService("ReplicatedStorage"):WaitForChild("ReplicaRemoteEvents"):WaitForChild("Replica_ReplicaSignal")
+
+local sendingAnswers = false
+local randomizedSending = false
+local delayTime = 1
+
+local function sendAllAnswers()
+    for _, answer in ipairs(answers) do
+        local args = { [1] = 2, [2] = "Answer", [3] = answer }
+        ReplicaSignal:FireServer(unpack(args))
+    end
+end
+
+-- no more warehouse
+
 local Window = Rayfield:CreateWindow({
    Name = "Chroma",
    LoadingTitle = "Open-sourced Roblox universal cheat.",
@@ -11,10 +29,46 @@ local Window = Rayfield:CreateWindow({
    KeySystem = false, 
 })
 
-local Tab = Window:CreateTab("Credits") 
-local Button = Tab:CreateButton({
+local Voice = Window:CreateTab("VC")
+Voice:CreateButton({
+      Name = "Unsuspend VC",
+      Info = "If VC banned, unsuspends your voice chat.",
+      Callback = function()
+         game:GetService("VoiceChatService"):joinVoice()
+      end,
+})
+
+local Credits = Window:CreateTab("Credits") 
+Credits:CreateButton({
    Name = "AsteroidLord",
    Info = "Owner and Developer of Chroma", 
    Callback = function()
+   end,
+})
+
+local Games = Window:CreateTab("Games") 
+Games:AddSection({
+      Name = "Type or Die",
+})
+
+Games:CreateToggle({
+    Name = "Auto Answer",
+    CurrentValue = false,
+    Callback = function(state)
+        sendingAnswers = state
+        task.spawn(function()
+            while sendingAnswers do
+                sendAllAnswers()
+                task.wait(delayTime)
+            end
+        end)
+    end
+})
+
+Games:CreateButton({
+   Name = "Answer",
+   Info = "Sends a answer", 
+   Callback = function()
+         sendAllAnswers()
    end,
 })
