8d135339009d46a86567decf55d02798e42f4fab
AsteroidLord <84286534+asteroidlordfr@users.noreply.github.com>
2025-11-02 01:54:28 -0400

    Update Chroma.lua

--- Source/Chroma.lua
+++ Source/Chroma.lua
@@ -1,13 +1,3 @@
---[
--- Chroma
---
--- A open-source Roblox Universal tool to tweak your gameplay to the max.
--- This code is licensed under the GNU General Public License (V3)
---
--- Have fun!
--- (Yes yes, some of this is GPT but only i'd say only 15/100 is GPT as I don't know much Lua.)
---]
-
 local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/asteroidlordfr/Chroma/main/Source/Library.lua'))()
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
@@ -28,7 +18,7 @@ local AnswersSent = false
 local Answers = {"treadmill","samsung","leopard","multiple","rectangle","americanfootball","wednesday","weddingdress","knife","Master bedroom","Cable stripping machine","Philip Sherman","The Great Wall of China","Michael Jordan","Girl With A Pearl Earring","Invincibility","Rectangular prism","Steering wheel","Daily dose of internet","Taking out the trash","scientific calculator","Tamago kake gohan","Amazon Prime Video","inflatable party decorations","Police Car","precious gemstone","sportswear","Air Conditioning","Flight Attendancy","Fire Extinguishers","red light green light","Physical Education","Sour Patch kids","hide and seek","Chocolate Chip Cookie Dough Ice Cream","Anna Sophia","Magdalena","Trinity","Patrick","explore the outdoors","Baby Princess Rosalina","Metallic Gasoline Blue Green","Playing with controlling toys","Malfunctioning Playstation Controller","Duke of Weselton","Waste","Buttercup","Granddaughter","Multipurpose Permanent Marker","Professional Development","Microwave Oven","Washing Machine","compression stockings","smoothie","Interactive whiteboard","Medium Density Fiberboard","Tent pole repair sleeve","International Space Station","Health Insurance","Burrowing Owl","Professional Racketball","Stand up Paddleboarding","Volcanic Eruption","Fairy Godmothers","Statistics and Probability","advanced interactive multidimensional modeling system","Identification Card","Limestone Egyptian Waterclock","Joystick controller","Baby Princess Rosalina","Chocolate Ice Cream Sandwich","Stand Up Paddleboarding","Mozzarella Cheese","Stand Up Paddleboarding","United States of America","Super Mario Brothers","Great White Shark","Pomegranate","Flat screen television","Wheel barrow","Centimeter","Dumbells","Christopher Robin","Sweet Potato","Cherry Blossom","Hippopotomonstrosesquippedaliophobia","Vitamin B12","gaming chair","Saxophone","Wisdom Teeth","Harley Quinn","Frozen Water Bottle","Hermit Crab","Galapagos tortoise","Mountain Everest","Macadamia Nuts","flower","rock","americancheese","steak","pig","angry","taylorswift","kreekcraft","refrigerator handle","Electric Bass Guitar","Rubber Duckie","German","colacola","apple","lemonade","toiletpaper","headphone","captainamerica","facebook","strawberry","mouth","television","united states of america","Construction","Condensed Milk","Cumulonimbus"}
 local aimbotEnabled = false
 local aimbotRightClick = false
-local lockPart = "Head"
+local wallCheck = false
 local teamCheck = false
 
 local function getClosestPlayer()
@@ -36,11 +26,17 @@ local function getClosestPlayer()
     local target
     local mouse = LocalPlayer:GetMouse()
     for _, plr in pairs(Players:GetPlayers()) do
-        if plr ~= LocalPlayer and plr.Character and plr.Character:FindFirstChild(lockPart) then
+        if plr ~= LocalPlayer and plr.Character and plr.Character:FindFirstChild("Head") then
             local humanoid = plr.Character:FindFirstChild("Humanoid")
             if humanoid and humanoid.Health > 0 then
                 if teamCheck and plr.Team == LocalPlayer.Team then continue end
-                local pos, onScreen = workspace.CurrentCamera:WorldToViewportPoint(plr.Character[lockPart].Position)
+                if wallCheck then
+                    local origin = workspace.CurrentCamera.CFrame.Position
+                    local direction = (plr.Character.Head.Position - origin)
+                    local raycast = workspace:Raycast(origin, direction, {LocalPlayer.Character})
+                    if raycast and raycast.Instance:IsDescendantOf(plr.Character) == false then continue end
+                end
+                local pos, onScreen = workspace.CurrentCamera:WorldToViewportPoint(plr.Character.Head.Position)
                 if onScreen then
                     local dist = (Vector2.new(pos.X, pos.Y) - Vector2.new(mouse.X, mouse.Y)).Magnitude
                     if dist < closestDist then
@@ -61,14 +57,13 @@ local function toggleAimbot(enable)
         aimbotConnection = RunService.RenderStepped:Connect(function()
             if aimbotRightClick and not UserInputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton2) then return end
             local target = getClosestPlayer()
-            if target and target.Character and target.Character:FindFirstChild(lockPart) then
+            if target and target.Character and target.Character:FindFirstChild("Head") then
                 local cam = workspace.CurrentCamera
-                cam.CFrame = CFrame.new(cam.CFrame.Position, target.Character[lockPart].Position)
+                cam.CFrame = CFrame.new(cam.CFrame.Position, target.Character.Head.Position)
             end
         end)
     end
 end
-
 
 local Window = Library:CreateWindow({
    Name = "🟢 Chroma",
@@ -93,13 +88,13 @@ Movement:CreateButton({Name = "Reset Walkspeed", Callback = function() Humanoid.
 Movement:CreateButton({Name = "Reset Jump Power", Callback = function() Humanoid.JumpPower = defaultJumpPower end})
 
 local Cheats = Window:CreateTab("Cheats")
-
 Cheats:CreateToggle({
     Name = "Aimbot [RIGHT CLICK]",
     CurrentValue = false,
     Callback = function(state)
         aimbotRightClick = state
         if state then aimbotEnabled = false end
+        toggleAimbot(state or aimbotEnabled)
     end
 })
 
@@ -109,15 +104,15 @@ Cheats:CreateToggle({
     Callback = function(state)
         aimbotEnabled = state
         if state then aimbotRightClick = false end
+        toggleAimbot(state or aimbotRightClick)
     end
 })
 
-Cheats:CreateDropdown({
-    Name = "Lock Part",
-    Options = {"Head","HumanoidRootPart","Torso","UpperTorso","LowerTorso"},
-    CurrentOption = lockPart,
-    Callback = function(option)
-        lockPart = option
+Cheats:CreateToggle({
+    Name = "Wall Check",
+    CurrentValue = false,
+    Callback = function(state)
+        wallCheck = state
     end
 })
 
